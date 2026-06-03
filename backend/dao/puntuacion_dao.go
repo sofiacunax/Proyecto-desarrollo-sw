324fadd393b7f610dbc36343a18da27311c5e233
sofiacuna <2414646@ucc.edu.ar>
2026-06-03 17:26:39 -0300

    Agregar DAO de puntuaciones

---
+++ backend/dao/puntuacion_dao.go
@@ -0,0 +1,81 @@
+package dao
+
+import (
+	"proyecto-desarrollo-sw/backend/db"
+	"proyecto-desarrollo-sw/backend/models"
+)
+
+type PuntuacionDAO struct{}
+
+func NewPuntuacionDAO() *PuntuacionDAO {
+	return &PuntuacionDAO{}
+}
+
+func (dao *PuntuacionDAO) CrearPuntuacion(puntuacion models.Puntuacion) error {
+	query := `
+		INSERT INTO puntuaciones
+		(usuario_id, evento_id, puntuacion)
+		VALUES (?, ?, ?)
+	`
+
+	_, err := db.DB.Exec(
+		query,
+		puntuacion.UsuarioID,
+		puntuacion.EventoID,
+		puntuacion.Puntuacion,
+	)
+
+	return err
+}
+
+func (dao *PuntuacionDAO) ObtenerPuntuacionesPorEvento(eventoID int) ([]models.Puntuacion, error) {
+	query := `
+		SELECT id, usuario_id, evento_id, puntuacion
+		FROM puntuaciones
+		WHERE evento_id = ?
+	`
+
+	rows, err := db.DB.Query(query, eventoID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var puntuaciones []models.Puntuacion
+
+	for rows.Next() {
+		var puntuacion models.Puntuacion
+
+		err := rows.Scan(
+			&puntuacion.ID,
+			&puntuacion.UsuarioID,
+			&puntuacion.EventoID,
+			&puntuacion.Puntuacion,
+		)
+
+		if err != nil {
+			return nil, err
+		}
+
+		puntuaciones = append(puntuaciones, puntuacion)
+	}
+
+	return puntuaciones, nil
+}
+
+func (dao *PuntuacionDAO) ObtenerPromedioPorEvento(eventoID int) (float64, error) {
+	query := `
+		SELECT COALESCE(AVG(puntuacion), 0)
+		FROM puntuaciones
+		WHERE evento_id = ?
+	`
+
+	var promedio float64
+
+	err := db.DB.QueryRow(query, eventoID).Scan(&promedio)
+	if err != nil {
+		return 0, err
+	}
+
+	return promedio, nil
+}
