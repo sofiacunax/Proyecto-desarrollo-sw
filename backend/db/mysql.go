ca831f1d00a3275d0fd18c609e4cd40dfc336618
sofiacuna <2414646@ucc.edu.ar>
2026-06-22 21:53:33 -0300

    feat: dockerizacion del proyecto

--- backend/db/mysql.go
+++ backend/db/mysql.go
@@ -1,7 +1,10 @@
 package db
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"proyecto-desarrollo-sw/backend/models"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -11,23 +14,42 @@ var DB *gorm.DB
 
 func Connect() {
 	var err error
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		getEnv("DB_USER", "root"),
+		getEnv("DB_PASSWORD", "Popia115a"),
+		getEnv("DB_HOST", "localhost"),
+		getEnv("DB_PORT", "3306"),
+		getEnv("DB_NAME", "proyecto_desarrollo_sw"),
+	)
 
-	DB, err = gorm.Open(mysql.Open(
-		"root:SasukeKira2006.@tcp(localhost:3306)/proyecto_desarrollo_sw?charset=utf8mb4",
-	), &gorm.Config{})
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	/*if err = DB.AutoMigrate(
+	if err = DB.AutoMigrate(
 		&models.Usuario{},
 		&models.Evento{},
 		&models.Entrada{},
 		&models.Puntuacion{},
 	); err != nil {
 		log.Fatal(err)
-	}*/
+	}
+
+	if err = DB.Exec("ALTER TABLE eventos MODIFY horario TIME NOT NULL").Error; err != nil {
+		log.Fatal(err)
+	}
 
 	log.Println("Conectado a MySQL y esquema migrado")
+}
+
+func getEnv(key, defaultValue string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return defaultValue
+	}
+
+	return value
 }
